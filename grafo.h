e624545dcb7e8236b560040bb324da8432f7f287
enzogomes2 <121980000+enzogomes2@users.noreply.github.com>
2023-01-04 21:45:22 -0300

    Update grafo.h

--- grafo.h
+++ grafo.h
@@ -19,8 +19,6 @@ struct arco *insere_adjacencia(struct arco *ini, char *vert_dest, int peso);
 int insere_arco(grafo gf, char *vert1, char *vert2, int peso);
 void insere_novo_vertice(grafo gf, char *val);
 int eh_vertice(grafo gf, char *vert);
-void mostra_lista(grafo gf);
-void busca_largura(grafo g, char *vert_origem);
 lista letraC(grafo g, char *vert_origem, int K);
 void letraD(char* nome, grafo g, lista* l);
 void letraE(grafo g, char* origem, char* destino);
